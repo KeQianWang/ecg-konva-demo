174b66723a55aa5cf3442a65682ffbc99d9c5452
hejunji <junji.he@biomind.ai>
2022-12-07 13:58:20 +0800

    chore: stage width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,69 @@
 import styles from './index.less';
-import { Stage, Layer, Text, Line, Group,Circle } from "react-konva";
+import { Stage, Layer, Text, Line, Group, Circle } from 'react-konva';
 // @ts-ignore
-import { pointsData }  from "@/utils/utils";
+import { pointsData } from '@/utils/utils';
 export default function IndexPage() {
-  const scale = 1;// 倍数阈值
+  const scale = 1; // 倍数阈值
   const step = 0.3 * scale; // 波线点间距阈值
   const sampleRate = 500; //采样率（每秒采集数
   const voltage = 0.002; //电压
   // X倍数阈值
-  const gridSizeX = 6;//每小格（X轴 0.04/秒，每秒25个
+  const gridSizeX = 6; //每小格（X轴 0.04/秒，每秒25个
   // Y倍数阈值
-  const gridSizeY = 6;//每小格（Y轴 0.1/mv，1mv是2个大格
+  const gridSizeY = 6; //每小格（Y轴 0.1/mv，1mv是2个大格
 
+  const width = ~~(pointsData.length / sampleRate) * 25 * gridSizeX;
   let init = {
-    width: ~~(pointsData.length / sampleRate) * 25 * gridSizeX,
-    height: 151,
+    width,
+    stageWidth: Math.max(width, window.innerWidth - 10),
+    height: 250,
   };
 
   // 垂直线
-  const drawGirdX = () =>{
-    const width = pointsData.length * gridSizeX * +(init.width / pointsData.length).toFixed(2);
+  const drawGirdX = () => {
+    const width =
+      pointsData.length *
+      gridSizeX *
+      +(init.width / pointsData.length).toFixed(2);
     const height = 180;
     let items = [];
     for (let x = 0, g = 0; x <= width; x += ~~gridSizeX, g++) {
       items.push(
         <Line
-        key={x}
-        points={[x, 0, x, height]}
-        stroke={g % 5 === 0 ? "#ADD5B5" : "#D0E8D5"}
-        strokeWidth={1}
-        offsetX={-0.5}
-        />
-      )
+          key={x}
+          points={[x, 0, x, height]}
+          stroke={g % 5 === 0 ? '#ADD5B5' : '#D0E8D5'}
+          strokeWidth={1}
+          offsetX={-0.5}
+        />,
+      );
     }
-    return items
-  }
+    return items;
+  };
   // 水平线（高度不随着倍数变化
   const drawGirdY = () => {
-    const width = pointsData.length * gridSizeX * +(init.width / pointsData.length).toFixed(2);
+    const width =
+      pointsData.length *
+      gridSizeX *
+      +(init.width / pointsData.length).toFixed(2);
     const height = 180;
     let items = [];
-     for (let y = 0, f = 0; y <= height; y += ~~gridSizeY, f++) {
-       items.push(
-         <Line
-         key={y}
-         points={[0, y, width, y]}
-         stroke={y % 5 === 0 ? "#ADD5B5" : "#D0E8D5"}
-         strokeWidth={1}
-         offsetX={-0.5}
-        />
-       )
+    for (let y = 0, f = 0; y <= height; y += ~~gridSizeY, f++) {
+      items.push(
+        <Line
+          key={y}
+          points={[0, y, width, y]}
+          stroke={y % 5 === 0 ? '#ADD5B5' : '#D0E8D5'}
+          strokeWidth={1}
+          offsetX={-0.5}
+        />,
+      );
     }
-     return items
-  }
+    return items;
+  };
 
-  const drawLine = () =>{
-    let points:any = [];
+  const drawLine = () => {
+    let points: any = [];
     const width = init.width;
     let positionX = +(width / pointsData.length).toFixed(2);
     let positionY = voltage * 10 * gridSizeY;
@@ -65,14 +73,13 @@ export default function IndexPage() {
       points.push(x);
       points.push(y);
     }
-    return points
-  }
-
+    return points;
+  };
 
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
-      <Stage width={init.width} height={init.height}>
+      <Stage width={init.stageWidth} height={init.height}>
         <Layer>
           <Group name="groupGrid">
             {drawGirdX()}
@@ -81,11 +88,7 @@ export default function IndexPage() {
         </Layer>
         <Layer>
           <Group name="groupLine">
-            <Line
-              points={drawLine()}
-              strokeWidth={1}
-              stroke={"#00000"}
-            />
+            <Line points={drawLine()} strokeWidth={1} stroke={'#00000'} />
           </Group>
         </Layer>
       </Stage>
